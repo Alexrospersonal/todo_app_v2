6be6aac4bc548b83d47fb5e8de44b1e55b06d803
Alexrospersonal <alexrospersonal@gmail.com>
2025-01-03 14:08:13 +0200

    Add to display description text in TodoCard

--- lib/todos/widgets/todos_list/todos_list.dart
+++ lib/todos/widgets/todos_list/todos_list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_quill/quill_delta.dart';
 import 'package:todo_app_v2/common/utils/date_formatter.dart';
 import 'package:todo_app_v2/edit_task/view/view.dart';
 import 'package:todo_app_v2/l10n/l10n.dart';
@@ -11,40 +14,72 @@ import 'package:todos_repository/todos_repository.dart';
 class TodosList extends StatelessWidget {
   const TodosList({super.key});
 
+  bool _buildNewWidgetWhen(TodosState previous, TodosState current) {
+    if (previous.todos.length != current.todos.length) {
+      return true;
+    }
+
+    if (previous.filter != current.filter) {
+      return true;
+    }
+
+    if (previous.selectedCategory != current.selectedCategory) {
+      return true;
+    }
+
+    return true;
+  }
+
+  Widget _buildWidgetIfTasksIsEmpty(TodosState state, AppLocalizations l10n) {
+    if (state.status == TodosOverviewStatus.loading) {
+      return const Center(child: CupertinoActivityIndicator());
+    } else if (state.status != TodosOverviewStatus.success) {
+      return const SizedBox.shrink();
+    } else {
+      return Center(
+        child: Text(l10n.addAnewTask),
+      );
+    }
+  }
+
+  List<Widget> _createTaskCartForExpansionTile(
+    Iterable<TaskEntity> tasks,
+    BuildContext context,
+  ) {
+    return tasks
+        .map(
+          (task) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5),
+            child: TodoCard(
+              task: task,
+              onTap: () => Navigator.of(context).push(
+                EditTaskPage.route(initialTask: task),
+              ),
+              category: task.category.value,
+              title: task.title,
+              dateTime: task.taskDate,
+              isNotification: task.notificationId != null || false,
+              repeatDuringWeek: task.repeatDuringWeek ?? [],
+              repeatDuringDay: task.repeatDuringDay,
+              isImportant: task.important,
+              color: task.color ?? baseColor.value,
+              isFinish: task.isFinished,
+            ),
+          ),
+        )
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TodosBloc, TodosState>(
-      buildWhen: (previous, current) {
-        if (previous.todos.length != current.todos.length) {
-          return true;
-        }
-
-        if (previous.filter != current.filter) {
-          return true;
-        }
-
-        if (previous.selectedCategory != current.selectedCategory) {
-          return true;
-        }
-
-        return true;
-      },
+      buildWhen: _buildNewWidgetWhen,
       builder: (context, state) {
         final l10n = context.l10n;
 
         if (state.todos.isEmpty) {
-          if (state.status == TodosOverviewStatus.loading) {
-            return const Center(child: CupertinoActivityIndicator());
-          } else if (state.status != TodosOverviewStatus.success) {
-            return const SizedBox.shrink();
-          } else {
-            return Center(
-              child: Text(l10n.addAnewTask),
-            );
-          }
+          return _buildWidgetIfTasksIsEmpty(state, l10n);
         }
-
-        // TODO: Рефактор коду та додати візуальне оформлення для ExpansionTile
 
         final newTasks = state.newTodos;
         final finishedTasks = state.finishedTodos;
@@ -87,56 +122,14 @@ class TodosList extends StatelessWidget {
                     title: Text(l10n.finishedTasksTitle),
                     tilePadding: EdgeInsets.zero,
                     childrenPadding: const EdgeInsets.symmetric(vertical: 5),
-                    children: finishedTasks
-                        .map(
-                          (task) => Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: TodoCard(
-                              task: task,
-                              onTap: () => Navigator.of(context).push(
-                                EditTaskPage.route(initialTask: task),
-                              ),
-                              category: task.category.value,
-                              title: task.title,
-                              dateTime: task.taskDate,
-                              isNotification:
-                                  task.notificationId != null || false,
-                              repeatDuringWeek: task.repeatDuringWeek ?? [],
-                              repeatDuringDay: task.repeatDuringDay,
-                              isImportant: task.important,
-                              color: task.color ?? baseColor.value,
-                              isFinish: task.isFinished,
-                            ),
-                          ),
-                        )
-                        .toList(),
+                    children:
+                        _createTaskCartForExpansionTile(finishedTasks, context),
                   ),
                 if (overdueTasks.isNotEmpty)
                   ExpansionTile(
                     title: Text(l10n.overdueTasksTitle),
-                    children: overdueTasks
-                        .map<Padding>(
-                          (task) => Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: TodoCard(
-                              task: task,
-                              onTap: () => Navigator.of(context).push(
-                                EditTaskPage.route(initialTask: task),
-                              ),
-                              category: task.category.value,
-                              title: task.title,
-                              dateTime: task.taskDate,
-                              isNotification:
-                                  task.notificationId != null || false,
-                              repeatDuringWeek: task.repeatDuringWeek ?? [],
-                              repeatDuringDay: task.repeatDuringDay,
-                              isImportant: task.important,
-                              color: task.color ?? baseColor.value,
-                              isFinish: task.isFinished,
-                            ),
-                          ),
-                        )
-                        .toList(),
+                    children:
+                        _createTaskCartForExpansionTile(overdueTasks, context),
                   ),
               ],
             ),
@@ -261,6 +254,7 @@ class _TodoCardState extends State<TodoCard> {
                 TodoTextsColumn(
                   category: category.toString(),
                   title: widget.title,
+                  description: widget.task.notate ?? '',
                   date: DateFormatter.formatDateTime(
                     context,
                     widget.dateTime,
@@ -291,6 +285,7 @@ class TodoTextsColumn extends StatelessWidget {
     required this.category,
     required this.title,
     required this.date,
+    required this.description,
     this.isNotification = false,
     this.repeatDuringWeek = '',
     this.repeatDuringDay = '',
@@ -299,6 +294,7 @@ class TodoTextsColumn extends StatelessWidget {
 
   final String category;
   final String title;
+  final String description;
   final String date;
   final bool isNotification;
 
@@ -309,6 +305,20 @@ class TodoTextsColumn extends StatelessWidget {
     return flag != null && flag
         ? Theme.of(context).colorScheme.onPrimary
         : Theme.of(context).colorScheme.surface;
+  }
+
+  String extractPlainText(String jsonString) {
+    final json = jsonDecode(jsonString) as List<dynamic>;
+    final buffer = StringBuffer();
+
+    for (final operation in json) {
+      operation as Map;
+      if (operation.containsKey('insert')) {
+        buffer.write(operation['insert']);
+      }
+    }
+
+    return buffer.toString().trim();
   }
 
   @override
@@ -327,6 +337,11 @@ class TodoTextsColumn extends StatelessWidget {
             style: Theme.of(context).textTheme.labelSmall!.copyWith(height: 1),
           ),
           Text(
+            extractPlainText(description),
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(height: 1),
+          ),
+          Text(
             date,
             style: const TextStyle(
               fontSize: 12,
@@ -341,43 +356,61 @@ class TodoTextsColumn extends StatelessWidget {
               const SizedBox(
                 width: 3,
               ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.repeat,
-                    color: getIconColor(
-                      context,
-                      flag: repeatDuringWeek.isNotEmpty,
-                    ),
-                  ),
-                  Text(
-                    repeatDuringWeek,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
+              TodoTextsColumnRowWidget(
+                iconData: Icons.repeat,
+                flag: repeatDuringWeek.isNotEmpty,
+                text: repeatDuringWeek,
               ),
               const SizedBox(
                 width: 3,
               ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.av_timer_sharp,
-                    color: getIconColor(
-                      context,
-                      flag: repeatDuringDay.isNotEmpty,
-                    ),
-                  ),
-                  Text(
-                    repeatDuringDay,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
+              TodoTextsColumnRowWidget(
+                iconData: Icons.av_timer_sharp,
+                flag: repeatDuringDay.isNotEmpty,
+                text: repeatDuringDay,
               ),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class TodoTextsColumnRowWidget extends StatelessWidget {
+  const TodoTextsColumnRowWidget({
+    required this.iconData,
+    required this.flag,
+    required this.text,
+    super.key,
+  });
+
+  final bool flag;
+  final String text;
+  final IconData iconData;
+
+  Color getIconColor(BuildContext context, {bool? flag}) {
+    return flag != null && flag
+        ? Theme.of(context).colorScheme.onPrimary
+        : Theme.of(context).colorScheme.surface;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          iconData,
+          color: getIconColor(
+            context,
+            flag: flag,
+          ),
+        ),
+        Text(
+          text,
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+      ],
     );
   }
 }
